295226424681aaa222601ff2678bd9c887fad09a
asmaasabet <asmaasabet28@gmail.com>
2024-12-20 01:10:10 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+// var age = prompt("How Old Are You?");
+// // document.write("Your Age Is"+" "+age);
+// function fullname(fname ,sname ,age){
+//     var full= fname +sname;
+//     var ageperdayes =age *365;
+//     document.write("hello Miss" +full +"YourAgePerDaysIs" +" " +ageperdays +"<br>");
+
+// }
+// // fullname("asmaa" ,"sabet" ,28);
+// function transfer(){
+   
+//     var result= 100 *50 ;
+//     return result;
+// }
+// function tax(){
+    
+
+//     document.write("<h1> salary now is : " + (transfer() - (transfer() *0.20)) +"LE</h1>")
+// }
+// tax();
+
+// function calc (age){
+//     age +365 +24 +"hours"+ "<br>";
+//     return " <h1>Hello</h1>"
+// }
+// // document.write(calc(10))
+// function transfer(){
+//    var res =100 *50 ;
+//    return res;
+// }
+// function tax(){
+//     var result =transfer() -(transfer() * 0.20);
+//     document.write("<h1>YourSalaryIs :" +result + "LE</h1>")
+// }
+// // tax();
+// // document.body.style.backgroundColor ="blue";
+
+// document.getElementById("head1");
+// document.getElementById("para");
+
+// function dark(){
+//     document.body.style.backgroundColor ="black";
+//     head1.style.color="white";
+//     para.style.color="white";
+// }
+// function light(){/
+
+
+
+
+
+
+//     document.body.style.backgroundColor ="white"; 
+//     head1.style.color="black";
+//     para.style.color="black";
+    
+// // }
+// var users=["asmaa" ,"ali" ,"mona"];
+// console.log(users[0]);
+// console.log(users[1]);
+// console.log(users[2]);
+// // var name ="asmaa mohamed sabet";
+// var users=["ahmed", "ali", "hossam"];
+// console.log(users);
+// var number =10.11
+var article= prompt("pleas write your article?");
+document.write(article.slice(0, 20). toUpperCase());
+// function dark(){
+//     document.body.style.backgroundColor="black";
+// }
+// function light(){
+//     document.body.style.backgroundColor="white";
+
+// }
